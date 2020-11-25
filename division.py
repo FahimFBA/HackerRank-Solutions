aa18d14a51e33ee4c6afc8e77720e0b88d4f4017
FahimFBA <fahimbinamin@gmail.com>
2020-11-25 19:07:34 +0600

    Wednesday Tasks

---
+++ division.py
@@ -0,0 +1,5 @@
+a=int(input())
+b=int(input())
+# The first line should contain the result of integer division,  a//b . The second line should contain the result of float division,  a/b .
+print(a//b)
+print(a/b)
