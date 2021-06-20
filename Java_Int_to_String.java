28ef4d37d8c293f91e79222e99cb9f64173e182d
FahimFBA <fahimbinamin@gmail.com>
2021-06-20 12:10:42 +0600

    solved int to string in java

---
+++ Java_Int_to_String.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int value = sc.nextInt();
+        if (value >= -100 && value <= 100) {
+            System.out.println("Good job");
+        }
+
+    }
+}
